9c730683d1bb1fb108d0e638e8aa9f9d81edea01
Nate Hastings <z1929228@students.niu.edu>
2022-05-03 14:36:45 -0500

    Updated Core Repo

--- www/index.php
+++ www/index.php
@@ -17,7 +17,7 @@
             'Value' => $_COOKIE['PHPSESSID']
         )
     );
-    $cart = GetData('http://students.cs.niu.edu/~z1929228/csci466/group_project/www/Managers/CartManager.php', 'GET', )
+    $cart = GetData('http://students.cs.niu.edu/~z1929228/csci466/group_project/www/Managers/CartManager.php', 'GET', $cookies);
 
     // //sleep(1);
     // $url = "http://students.cs.niu.edu/~z1929228/csci466/group_project/www/Managers/CartManager.php";
