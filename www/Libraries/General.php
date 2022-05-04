943f112b6d90bd3958cb34a820c6c25132c9fcba
Nate Hastings <z1929228@students.niu.edu>
2022-05-03 21:39:09 -0500

    Updated Core Repo

--- www/Libraries/General.php
+++ www/Libraries/General.php
@@ -1,36 +1,39 @@
 <?php
 
-function GetData(string $URL, string $method, array $cookies=NULL, array $Payload=NULL)
+if(!function_exists('GetData'))
 {
-    $curl = curl_init($URL);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-    if($method == "POST")
+    function GetData(string $URL, string $method, array $cookies=NULL, array $Payload=NULL)
     {
-        curl_setopt($curl, CURLOPT_POST, true);  
-    }
+        $curl = curl_init($URL);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-    if(isset($Payload) && !empty($Payload))
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($Payload));
-
-    if(isset($_COOKIE['PHPSESSID']) && !empty($_COOKIE['PHPSESSID']))
-    {
-        $PHPSESS = $_COOKIE['PHPSESSID'];
-        curl_setopt($curl, CURLOPT_COOKIE, "PHPSESSID=$PHPSESS");
-    }
-
-    if(isset($cookies) && !empty($cookies))
-    {
-        foreach ($cookies as $key => $cookie) {
-            $cookieKey = $cookie['Key'];
-            $cookieValue = $cookie['Value'];
-
-            curl_setopt($curl, CURLOPT_COOKIE, "$cookieKey=$cookieValue");
+        if($method == "POST")
+        {
+            curl_setopt($curl, CURLOPT_POST, true);  
         }
-    }
 
-    $resp = curl_exec($curl);
-    return $resp;
+        if(isset($Payload) && !empty($Payload))
+            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($Payload));
+
+        if(isset($_COOKIE['PHPSESSID']) && !empty($_COOKIE['PHPSESSID']))
+        {
+            $PHPSESS = $_COOKIE['PHPSESSID'];
+            curl_setopt($curl, CURLOPT_COOKIE, "PHPSESSID=$PHPSESS");
+        }
+
+        if(isset($cookies) && !empty($cookies))
+        {
+            foreach ($cookies as $key => $cookie) {
+                $cookieKey = $cookie['Key'];
+                $cookieValue = $cookie['Value'];
+
+                curl_setopt($curl, CURLOPT_COOKIE, "$cookieKey=$cookieValue");
+            }
+        }
+
+        $resp = curl_exec($curl);
+        return $resp;
+    }
 }
 
 ?>
