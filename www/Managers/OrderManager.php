083daca0a9fe63b42f8c1a003d3b5d4ce04019e5
Nate Hastings <z1929228@students.niu.edu>
2022-05-03 21:11:11 -0500

    Updated Core Repo

--- www/Managers/OrderManager.php
+++ www/Managers/OrderManager.php
@@ -39,7 +39,7 @@ if($method == "GET")
             array_push($orderItems, array('ProductID' => $orderItem['Product_ID'], 'Quantity' => $orderItem['QTY']));
         }
 
-        $orders[$orderKey][] = $orderItem;
+        $orders[$orderKey][] = $orderItems;
     }
 
     $data = $orders;
