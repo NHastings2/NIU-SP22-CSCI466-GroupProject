9ae2b312aa0da23ac0c79c70d3268e665259a97f
David Lyons <35667959+lyonsdavi@users.noreply.github.com>
2022-05-04 21:50:55 -0500

    Added cart viewing/ordering

---
+++ www/Cart.php
@@ -0,0 +1,45 @@
+<html><head><title>Game Shop</title></head><body style="background-color:black; color:white;"><?php
+
+include "Libraries/Inventory.php";
+
+$cart = GetCart();
+$json = json_decode($cart, true);
+
+if (!empty($json)) {
+    echo "<table border bordercolor=\"white\"><tr><th>Name</th><th>Quantity Ordered</th><th>Price</th><th>Product Page</th><th>Update Product Count</th><th>Remove product</th></tr>";
+    $totalCost = 0;
+    foreach ($json as $key => $value) {
+        $item = GetInventoryItemByID($value["productID"]);
+        $itemJson = json_decode($item, true);
+
+        if (!empty($itemJson)) {
+            foreach ($itemJson as $itemKey => $itemValue) {
+                echo "<tr><td>{$itemValue["Product_Name"]}</td>
+                        <td>{$value["quantity"]}</td>
+                        <td>\${$itemValue["Product_Cost"]}</td>
+                        <td><a href=\"Item.php?ID={$value["Product_ID"]}\">Product page</a></td>
+                        <td><form method=\"POST\" action=\"./Managers/CartManager.php\">
+                                <input type=\"hidden\" name=\"Action\" value=\"Update\"/>
+                                <input type=\"hidden\" name=\"ProductID\" value=\"{$itemValue["Product_ID"]}\"/>
+                                <input type=\"number\" name=\"Quantity\" placeholder=\"Quantity\" min=\"1\" max=\"{$itemValue["Product_in_Stock"]}\"/>
+                                <input type=\"submit\" value=\"Change Order Amount\"/>
+                                <input type=\"hidden\" name=\"Redirect\" value=\"/Cart.php\"/></form></td>
+                        <td><form method=\"POST\" action=\"./Managers/CartManager.php\">
+                                <input type=\"hidden\" name=\"Action\" value=\"Remove\"/>
+                                <input type=\"hidden\" name=\"ProductID\" value=\"{$itemValue["Product_ID"]}\"/>
+                                <input type=\"submit\" value=\"Remove From Cart\"/>
+                                <input type=\"hidden\" name=\"Redirect\" value=\"/Cart.php\"/></form></td></tr>";
+                $totalCost += $value["quantity"] * $itemValue["Product_Cost"];
+            }
+        }
+    }
+    echo "</table>";
+    echo "Your current order will cost \$$totalCost<form method=\"POST\" action=\"./Managers/OrderManager.php\">
+            <input type=\"hidden\" name=\"Action\" value=\"Create\"/>
+            <input type=\"number\" name=\"CC_Num\" placeholder=\"Last 4 digits of your credit card\"/>
+            <input type=\"text\" name=\"ShippingAddress\" placeholder=\"Shipping address\"/>
+            <input type=\"number\" name=\"CustomerID\" placeholder=\"Customer ID\"/>
+            <input type=\"submit\" value=\"Order!\"</form>";
+}
+
+?></body></html>
