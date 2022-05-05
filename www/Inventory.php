620fec2a98fe4bd8d432409ec7376ab415708871
David Lyons <35667959+lyonsdavi@users.noreply.github.com>
2022-05-04 20:10:06 -0500

    Added inventory

--- www/Inventory.php
+++ www/Inventory.php
@@ -1,1 +1,19 @@
-{\rtf1}
+<html><head><title>NED Inventory</title></head><body><?php
+
+include "Libraries/Inventory.php";
+
+$inventory = GetInventoryItems();
+$json = json_decode($inventory, true);
+
+if (!empty($json)) {
+    echo "<table><tr><th>Name</th><th>Quantity</th><th>Price</th><th>Product Page</th></tr>";
+    foreach ($json as $key => $value) {
+        echo "<tr><td>{$value["Product_Name"]}</td>
+                <td>{$value["Product_in_Stock"]}</td>
+                <td>{$value["Product_Cost"]}</td>
+                <td><a href=\"Item.php?ID={$value["Product_ID"]}\">Product page</a></td></tr>";
+    }
+    echo "</table>";
+}
+
+?></body></html>
