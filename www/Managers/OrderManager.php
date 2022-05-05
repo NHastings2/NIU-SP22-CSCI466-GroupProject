1e1f2e841e8fea57fe2eedf7c23e45df351d39dc
Nate Hastings <z1929228@students.niu.edu>
2022-05-04 22:08:24 -0500

    Updated Core Repo

--- www/Managers/OrderManager.php
+++ www/Managers/OrderManager.php
@@ -72,7 +72,7 @@ else if($method == "POST")
                 $itemID = $item["productID"];
                 $storeItem = json_decode(GetData("https://students.cs.niu.edu/~z1929228/csci466/group_project/www/Managers/InventoryManager.php?ID=$itemID", "GET"));
 
-                
+                $postData = array('Action' => 'Update', 'ID' => $itemID, 'Quantity' => ($storeItem['']));
 
                 $Order_Total += ($storeItem['Product_Cost'] * $item['quantity']);
             }
@@ -87,7 +87,8 @@ else if($method == "POST")
                 ExecuteSQL("INSERT INTO ORDER_PRODUCTS (Order_ID, Product_ID, QTY) VALUES (?,?,?)", array($orderID, $item['productID'], $item['quantity']));
             }
 
-
+            $postData = array('Action' => 'Clear');
+            
 
             $data = ExecuteSQL("SELECT * FROM ORDERS WHERE Order_ID=?", array($orderID));
         }
