5dec7789cee7cf8d2fdc09849aaebf78a81e2edd
Nate Hastings <z1929228@students.niu.edu>
2022-05-04 20:22:02 -0500

    Updated Core Repo

--- www/Inventory.php
+++ www/Inventory.php
@@ -1,4 +1,4 @@
-<html><head><title>NED Inventory</title></head><body style="background-color:gray; color:white;"><?php
+<html><head><title>NED Inventory</title></head><body style="background-color:black; color:white;"><?php
 
 include "Libraries/Inventory.php";
 
