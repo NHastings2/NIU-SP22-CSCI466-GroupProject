0e9fc16796026d3bfb20b8b21ac2a83473b4b613
David Lyons <35667959+lyonsdavi@users.noreply.github.com>
2022-05-05 21:23:33 -0500

    Added total cost feature

--- www/Customer.php
+++ www/Customer.php
@@ -16,19 +16,23 @@ if (!empty($json)) {
         $orders = GetOrdersByCustomerID($value["Customer_ID"]);
         $ordersJson = json_decode($orders, true);
 
+        $totalCost = 0;
         if (!empty($ordersJson)) {
             echo "<p>Orders:</p><table border bordercolor=\"white\">";
             foreach ($ordersJson as $orderKey => $orderValue) {
                 echo "<tr><td>{$orderValue["Order_ID"]}</td>
                     <td>{$orderValue["Order_Date"]}</td>
                     <td>{$orderValue["Order_Status"]}</td>
+                    <td>{$orderValue["Total_Cost"]}</td>
                     <td>{$orderValue["Notes"]}</td>
                     <td><a href=\"Order.php?ID={$orderValue["Order_ID"]}\">Order page</a></td></tr>";
+                $totalCost += $value["Total_Cost"];
             }
             echo "</table>";
         } else {
             echo "<p>This customer has no orders.</p>";
         }
+        echo "<p>The total cost for all orders is: \${$orderValue["Total_Cost"]}";
     }
 } else {
     echo "That item does not exist!";
