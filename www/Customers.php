80c0e9d1c3704049fe54aa580d3620051967240a
David Lyons <35667959+lyonsdavi@users.noreply.github.com>
2022-05-05 21:12:07 -0500

    Update Customers.php

--- www/Customers.php
+++ www/Customers.php
@@ -8,10 +8,10 @@ $customers = GetCustomers();
 $json = json_decode($customers, true);
 
 if (!empty($json)) {
-    echo "<table border bordercolor=\"white\"><tr><th>Customer ID</th><th>Customer Name</th><th>Customer page</th></tr>";
+    echo "<table border bordercolor=\"white\"><tr><th>Customer Name</th><th>Customer ID</th><th>Customer page</th></tr>";
     foreach ($json as $key => $value) {
-        echo "<tr><td>{$value["Customer_ID"]}</td>
-                <td>{$value["Customer_Name"]}</td>
+        echo "<tr><td>{$value["Customer_Name"]}</td>
+                <td>{$value["Customer_ID"]}</td>
                 <td><a href=\"Customer.php?ID={$value["Customer_ID"]}\">Customer page</a></td></tr>";
     }
     echo "</table>";
